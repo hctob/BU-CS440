df0d40448c4b477dfba62a66880280ac761035cc
Dan Bottcher <dbottch1@binghamton.edu>
2019-09-13 03:03:01 -0400

    Got size(), empty(), and typename() to work in my own test file. Still need to make the 'constructor' take whatever that comparator is. Time for sleep though.

--- bottcher_prog1/Deque.hpp
+++ bottcher_prog1/Deque.hpp
@@ -1,23 +1,31 @@
 #ifndef BOTTCHER_DEQUE
 #define BOTTCHER_DEQUE
-#include <iostream>
+
+#include <cstring> /*strlen() for Deque.type_name*/
+
+#define str(a) #a
 
 #define Deque_DEFINE(db)     \
     struct Deque_##db##_it;     \
     struct Deque_##db {        \
         db *elements;           \
         size_t _size = 0;          \
-        int front = -1;         \
-        unsigned int back = 0;          \
-                                    \
+        unsigned int front_idx = 0;         \
+        unsigned int back_idx = 0;          \
+        /*Member Functions*/        \
         /*Modifiers*/               \
-        void (*push_front)(Deque_##db *, db);                            \
+        void (*push_front)(Deque_##db *, db);                  \
         /*Capacity:*/                  \
         bool (*empty)(Deque_##db *);  \
         size_t (*max_size)(Deque_##db *);   \
         size_t (*size)(Deque_##db *);   \
         void (*resize)(Deque_##db *, db);   \
+        char type_name[(strlen("Deque_"#db) + 1)]; \
     };                                          \
+    void Deque_##db##_dtor(Deque_##db *ye)      \
+    {   \
+        free(ye);   \
+    }   \
     bool empty(Deque_##db *ye)\
     {   \
         return(ye->size(ye) == 0);                \
@@ -28,11 +36,22 @@
     }\
     void Deque_##db##_push_front(Deque_##db *ye, db value)  \
     {   \
-        std::cout << __func__ << std::endl; \
+        printf("test"); \
     }   \
     void resize(Deque_##db *ye, db value)   \
     {   \
         ye->elements = (db*)malloc((ye->size(ye) + 1) * sizeof(db)); \
+    } \
+    void Deque_##db##_ctor(Deque_##db *ye)  \
+    {   \
+        Deque_##db ndq;        \
+        ndq._size = 0;  \
+        ndq.empty = &empty; \
+        ndq.size = &size; \
+        ndq.push_front = &Deque_##db##_push_front; \
+        /*ndq.type_name = (char*)malloc((strlen("Deque_"#db) + 1) * sizeof(char));*/ \
+        strcpy(ndq.type_name, "Deque_"#db); \
+        *ye = ndq;  \
     }
 
 #endif
--- bottcher_prog1/b-test.cpp
+++ bottcher_prog1/b-test.cpp
@@ -1,24 +1,51 @@
-#include <iostream>
+//#include <dlfcn.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <random>
+#include <unistd.h>
+#include <string.h>
+#include <algorithm>
+#include <typeinfo>
 #include "Deque.hpp"
 
-Deque_DEFINE(int);
+struct MyClass {
+    int id;
+    char name[10];
+};
+
+bool
+MyClass_less_by_id(const MyClass &o1, const MyClass &o2) {
+    return o1.id < o2.id;
+}
+
+bool
+MyClass_less_by_name(const MyClass &o1, const MyClass &o2) {
+    return strcmp(o1.name, o2.name) < 0;
+}
+
+void
+MyClass_print(const MyClass *o) {
+    printf("%d\n", o->id);
+    printf("%s\n", o->name);
+}
+
+Deque_DEFINE(MyClass)
+
 int main(int argc, char** argv) {
     //Deque_int *de;
     //de->empty = &empty;
     //de->push_front = Deque_int_push_front;
     //std::cout << "test" << std::endl;
-    Deque_int *de = (Deque_int*)malloc(sizeof(Deque_int));
-
-    de->empty = empty;
-    //std::cout << "test 2\n";
-    de->push_front = Deque_int_push_front;
-    de->size = size;
-    std::cout << de->size << std::endl;
-    //Deque_int *r_de = &de;
-    //(de->empty(de) == 0);
-    de->push_front(de, 5);
-    //std::cout << std::boolalpha << "IsEmpty? "  << de->empty(de) << std::endl;
-
+    Deque_MyClass deq;
+    Deque_MyClass_ctor(&deq);
+    printf("test 1\n");
+    assert(deq.size(&deq) == 0);
+    // size() should return a size_t.
+    assert(typeid(std::size_t) == typeid(decltype(deq.size(&deq))));
+    assert(deq.empty(&deq));
+    printf("---- %s, %d\n", deq.type_name, (int) sizeof(deq.type_name));
+    printf("yeet");
     return 0;
 }
